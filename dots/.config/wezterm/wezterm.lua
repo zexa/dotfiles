ee513b02c45a73505acc4b74630d9726eaea5b6f
Zexa <zexa@cock.li>
2023-09-08 10:29:11 +0300

    Add split pane keybinds

--- dots/.config/wezterm/wezterm.lua
+++ dots/.config/wezterm/wezterm.lua
@@ -1,11 +1,30 @@
 local wezterm = require 'wezterm'
 local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 config.enable_tab_bar = false
 config.window_padding = {
   left = 0,
   right = 0,
   top = 0,
   bottom = 0,
+}
+config.keys = {
+  {
+    key = '5',
+    mods = 'CMD',
+    action = wezterm.action.SplitPane {
+      direction = 'Bottom',
+    },
+  },
+  {
+    key = '"',
+    mods = 'CMD',
+    action = wezterm.action.SplitPane {
+      direction = 'Right',
+    },
+  },
 }
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 return config
