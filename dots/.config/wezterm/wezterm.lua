142f9f74913dc12adc4ced6d8b13ab823b5da76d
Augustinas K <zexa@users.noreply.github.com>
2023-09-26 16:49:04 +0300

    Add unix/win based configuration as default

--- dots/.config/wezterm/wezterm.lua
+++ dots/.config/wezterm/wezterm.lua
@@ -1,9 +1,5 @@
 local wezterm = require 'wezterm'
 local config = {}
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-config.enable_tab_bar = false
 config.window_padding = {
   left = 0,
   right = 0,
@@ -12,19 +8,67 @@ config.window_padding = {
 }
 config.keys = {
   {
-    key = '5',
-    mods = 'CMD',
+    key = '-',
+    mods = 'ALT',
     action = wezterm.action.SplitPane {
       direction = 'Down',
     },
   },
   {
-    key = '\'',
-    mods = 'CMD',
+    key = '\\',
+    mods = 'ALT',
     action = wezterm.action.SplitPane {
       direction = 'Right',
     },
   },
+  {
+    key = 't',
+    mods = 'ALT',
+    action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'UpArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'DownArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  },
+  {
+    key = ',',
+    mods = 'ALT',
+    action = wezterm.action.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, _, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  }, 
 }
+for i = 1, 4 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
+config.use_fancy_tab_bar = false
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 return config
